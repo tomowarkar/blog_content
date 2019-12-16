2041b2c3716a258524124c154958b62dd427f22c
tomowarkar <3104kawamoto@gmail.com>
2019-12-16 19:51:06 +0900

    feat: piano

---
+++ piano/script.js
@@ -0,0 +1,11 @@
+const keys = document.querySelectorAll('.key')
+
+keys.forEach(key=>{
+  key.addEventListener('click', ()=>playNote(key))
+})
+
+function playNote(key) {
+  const noteAudio = document.getElementById(key.dataset.note)
+  noteAudio.currentTime = 0
+  noteAudio.play()
+}
